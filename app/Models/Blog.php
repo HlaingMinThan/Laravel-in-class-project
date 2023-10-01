06c50e6bce20ad77b5d96916653899fcdde3bc70
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-10-01 15:37:43 +0630

    laravel-day-4

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+
+    // protected $fillable = ['title', 'slug', 'intro', 'body'];
+    // protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
 use App\Models\Blog;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    //solution query = Blog::with('category')->latest()->get()
+    //first query = Blog::latest()->get()
     return view('home', [
-        'blogs' => Blog::all()
+        'blogs' => Blog::with('category')->latest()->get()
     ]);
 });
 
-Route::get('/blogs/{slug}', function ($slug) {
+Route::get('/blogs/{blog:slug}', function (Blog $blog) {
     return view('blog', [
-        'blog' => Blog::where('slug', $slug)->first()
+        'blog' => $blog
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('home', [
+        'blogs' => $category->blogs->load('category')
     ]);
 });
